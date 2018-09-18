5de70cc8a61d2c17b1b3a53ed42dedfba7ec9660
Happypig375 <hadrianwttang@outlook.com>
2018-09-18 22:27:29 +0800

    Testing 2

--- src/WindowsFormsApp1/Form1.cs
+++ src/WindowsFormsApp1/Form1.cs
@@ -3,9 +3,11 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Text;
 using System.Windows.Forms;
 using PixelFarm.Drawing;
+using Typography.FontManagement;
 
 namespace WindowsFormsApp1
 {
@@ -14,18 +16,28 @@ namespace WindowsFormsApp1
         public Form1()
         {
             InitializeComponent();
-            var view = new SkiaSharp.Views.Desktop.SKControl();
+            PixelFarm.Drawing.Skia.SkiaGraphicsPlatform.SetInstalledTypefaceProvider(
+                InstalledTypefaceCollection.GetSharedTypefaceCollection(collection =>
+                    collection.AddFontStreamSource(new FontSource())));
+            var view = new SkiaSharp.Views.Desktop.SKControl
+            { Dock = DockStyle.Fill };
             view.PaintSurface += (sender, e) =>
             {
-                using (var board = new PixelFarm.Drawing.Skia.MySkiaDrawBoard(0, 0, 0, 0, 1000, 1000))
+                using (var board = new PixelFarm.Drawing.Skia.MySkiaDrawBoard(0, 0, 0, 0, e.Info.Width, e.Info.Height))
                 {
                     board.CurrentTextColor = new PixelFarm.Drawing.Color(0, 0, 0);
-                    board.CurrentFont = new RequestFont("Arial", 30);
-                    board.DrawText(new[] { 'a', 'b', 'c' }, 0, 0);
+                    board.CurrentFont = new RequestFont("Times New Roman", 30);
+                    board.DrawText(new[] { 'a', 'b', 'c' }, 0, e.Info.Height);
                     e.Surface.Canvas.DrawBitmap(board.BackBmp, 0, 0);
                 }
             };
             Controls.Add(view);
+        }
+        class FontSource : IFontStreamSource
+        {
+            public string PathName => @"C:\Windows\Fonts\times.ttf";
+
+            public Stream ReadFontStream() => new FileStream(PathName, FileMode.Open, FileAccess.Read);
         }
     }
 }
